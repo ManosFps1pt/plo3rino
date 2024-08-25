797967fae86e519ede00d44058d2ff7cbd7802b7
ManosFps1pt <manos.dragasakis@gmail.com>
2024-08-25 22:17:49 +0300

    small changes

--- client.py
+++ client.py
@@ -2,14 +2,16 @@ import socket
 
 HOST = "192.168.1.78"
 PORT = 4070
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-print("opening")
+
 with open("gcode", "r") as file:
     doc = file.read()
-    print(doc)
+    client.send(doc.encode("UTF-8"))
+client.send(DISCONNECT_MESSAGE.encode("UTF-8"))
 
-client.send("hi bro, what's up?".encode("UTF-8"))
-print(client.recv(1024).decode("UTF-8"))
+
+
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 4070
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -10,8 +11,14 @@ server.listen(1)
 
 communication_socket, address = server.accept()
 print(f"connected to {address}")
-message = communication_socket.recv(1024).decode("UTF-8")
-print(f"message from {address} is {message}")
-communication_socket.send(f"thx for sending".encode("UTF-8"))
-communication_socket.close()
-print(f"connection with {address} closed")
+
+while True:
+    message = communication_socket.recv(1024).decode("UTF-8")
+    if message == DISCONNECT_MESSAGE:
+        communication_socket.close()
+        print(f"connection with {address} closed")
+        break
+    print(f"message from {address} is {message}")
+
+
+
